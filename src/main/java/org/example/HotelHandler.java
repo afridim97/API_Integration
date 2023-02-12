8cabb1637220f96710ad1028e8be53938d2cfee9
Afridi Majeed <69045000+afridim97@users.noreply.github.com>
2023-02-13 00:15:30 +0530

    GET API

--- src/main/java/org/example/HotelHandler.java
+++ src/main/java/org/example/HotelHandler.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.regions.Regions;
@@ -8,19 +10,24 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 
-public class HotelHandler implements RequestHandler<String, String>{
+public class HotelHandler implements RequestHandler<String, String>,RequestStreamHandler{
     private AmazonDynamoDB amazonDynamoDB;
-
+    private JSONParser parser = new JSONParser();
     private static final String DYNAMODB_TABLE_NAME = "Hotels";
     private Regions REGION = Regions.AP_SOUTH_1;
     Gson gson = new GsonBuilder().setPrettyPrinting().create();
-    public String handleRequest(String hotelCreateRequest, Context context)
+    public String handlePutHotel(String hotelCreateRequest, Context context)
     {   if(hotelCreateRequest == null){
-            return "null??";
+            return "Please enter valid details to add the hotel information";
         }
 
         this.initDynamoDbClient();
@@ -33,6 +40,62 @@ public class HotelHandler implements RequestHandler<String, String>{
         return persistData(hotel);
     }
 
+    public void handleGetByParam(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        JSONObject responseJson = new JSONObject();
+
+        this.initDynamoDbClient();
+        DynamoDB dynamoDb = new DynamoDB(amazonDynamoDB);
+        Item result = null;
+        try {
+            JSONObject event = (JSONObject) parser.parse(reader);
+            JSONObject responseBody = new JSONObject();
+
+            if (event.get("pathParameters") != null) {
+
+                JSONObject pps = (JSONObject) event.get("pathParameters");
+                if (pps.get("Name") != null) {
+
+                    String hotelName = (String) pps.get("Name");
+                    result = dynamoDb.getTable(DYNAMODB_TABLE_NAME)
+                            .getItem("Name", hotelName);
+                }
+
+            } else if (event.get("queryStringParameters") != null) {
+
+                JSONObject qps = (JSONObject) event.get("queryStringParameters");
+                if (qps.get("Name") != null) {
+
+                    String hotelName = (String) qps.get("Name");
+                    result = dynamoDb.getTable(DYNAMODB_TABLE_NAME)
+                            .getItem("Name", hotelName);
+                }
+            }
+            if (result != null) {
+                responseBody.put("Hotel", result.toJSONPretty());
+                responseJson.put("statusCode", 200);
+            } else {
+
+                responseBody.put("message", "No item found");
+                responseJson.put("statusCode", 404);
+            }
+
+            JSONObject headerJson = new JSONObject();
+            headerJson.put("x-custom-header", "my custom header value");
+
+            responseJson.put("headers", headerJson);
+            responseJson.put("body", responseBody.toString());
+
+        } catch (ParseException pex) {
+            responseJson.put("statusCode", 400);
+            responseJson.put("exception", pex);
+        }
+
+        OutputStreamWriter writer = new OutputStreamWriter(outputStream, "UTF-8");
+        writer.write(responseJson.toString());
+        writer.close();
+    }
     private String persistData(Hotel hotelCreateRequest){
 
         Map<String,AttributeValue> attributeValueMap = new HashMap<String, AttributeValue>();
@@ -72,5 +135,15 @@ public class HotelHandler implements RequestHandler<String, String>{
             System.err.println(e.getMessage() + "\n " + "Stack Trace: " + e.getStackTrace());
         }
 
+    }
+
+    @Override
+    public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
+
+    }
+
+    @Override
+    public String handleRequest(String s, Context context) {
+        return null;
     }
 }
